c64c668e4e0682955bed1ec2868f3004bd1be8e6
yunju-m <dbswnfnal@naver.com>
2023-05-29 18:28:26 +0900

    add chart.js

--- djangoproject/static/js/chart.js
+++ djangoproject/static/js/chart.js
@@ -1,69 +1,69 @@
 
 const ctx = document.getElementById("myChart");
-// const data = {
-//   datasets: [{
-//     label: 'Safety',
-//     data: [80, 20],
-//     backgroundColor: [
-//       '#9DCEFF',
-//       '#F2F3F6'
-//     ],
-//     scaleBeginAtZero: true,
-//     borderWidth:0,
-//     hoverOffset: 2,
-//   }]
-// }
-// const options = {
-//   cutout: '50%',
-// }
-// const myChart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data,
-//     options,
-// });
-
-var myDoughnutChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: data,
-  options: {
-    plugins: {
-      doughnutlabel: {
-        labels: [
-          {
-            text: 'The title',
-            font: {
-              size: '60'
-            }
-          },
-          {
-            text: getTotal,
-            font: {
-              size: '50'
-            },
-            color: 'grey'
-          },
-          {
-            text: '$100.000',
-            font: {
-              size: '30'
-            },
-            color: 'red'
-          },
-          {
-            text: '95%',
-            font: {
-              size: '45'
-            },
-          color: 'green'
-          }
-        ]
-      }
-    }		
-  }
+const data = {
+  datasets: [{
+    label: 'Safety',
+    data: [80, 20],
+    backgroundColor: [
+      '#9DCEFF',
+      '#F2F3F6'
+    ],
+    scaleBeginAtZero: true,
+    borderWidth:0,
+    hoverOffset: 2,
+  }]
+}
+const options = {
+  cutout: '50%',
+}
+const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data,
+    options,
 });
 
-var getTotal = function(myDoughnutChart) {
-    var sum = myDoughnutChart.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
-    return `Total: ${sum}`;
-}
+// var myDoughnutChart = new Chart(ctx, {
+//   type: 'doughnut',
+//   data: data,
+//   options: {
+//     plugins: {
+//       doughnutlabel: {
+//         labels: [
+//           {
+//             text: 'The title',
+//             font: {
+//               size: '60'
+//             }
+//           },
+//           {
+//             text: getTotal,
+//             font: {
+//               size: '50'
+//             },
+//             color: 'grey'
+//           },
+//           {
+//             text: '$100.000',
+//             font: {
+//               size: '30'
+//             },
+//             color: 'red'
+//           },
+//           {
+//             text: '95%',
+//             font: {
+//               size: '45'
+//             },
+//           color: 'green'
+//           }
+//         ]
+//       }
+//     }		
+//   }
+// });
+
+// var getTotal = function(myDoughnutChart) {
+//     var sum = myDoughnutChart.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
+//     return `Total: ${sum}`;
+// }
 
