0f76ac9f1ef1d3e79aa8f2452cfbf20bf16a3ab7
syrose01 <85944233+syrose01@users.noreply.github.com>
2023-06-06 20:22:47 +0900

    ㅁ

--- djangoproject/static/js/home.js
+++ djangoproject/static/js/home.js
@@ -4,7 +4,6 @@ const fileBtn = document.getElementById("fileBtn");
 const submitBtn = document.getElementById("submitBtn");
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
-//let file = document.getElementById("input-file")
 
 // 파일 업로드 시 제출 버튼 생성 함수
 function handleFileUp(event) {
@@ -33,33 +32,33 @@ submitBtn.addEventListener("mouseleave", handleBtnLeave);
 
 fileInput.addEventListener("input", handleFileUp);
 
-// //파일 넣는 부분에 이벤트가 발생했을 때 
-// file.onchange = function (e) {
-//     modal.classList.toggle('show');//모달창을 띄운다.
-// }
+//파일 넣는 부분에 이벤트가 발생했을 때 
+fileInput.onchange = function (e) {
+    modal.classList.toggle('show');//모달창을 띄운다.
+}
 
-// //body에 드래그하고 있을 때
-// body.addEventListener('dragover', function (e) {
-//     e.preventDefault();
-//     console.log('dragover');
-//     modal.classList.add('show');//모달창을 띄운다. add가 아닌 toggle로 하면 모달창이 껏다 켜졌다 반복함.
-// });
+//body에 드래그하고 있을 때
+body.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    console.log('dragover');
+    modal.classList.add('show');//모달창을 띄운다. add가 아닌 toggle로 하면 모달창이 껏다 켜졌다 반복함.
+});
 
-// //body에 드래그 드랍했을 때
-// body.addEventListener('drop', function (e) {
-//     e.preventDefault();
-//     console.log('drop');
-//     //버튼 글자 'Upload File'에서 'Submit'으로 바꾸기
-// });
+//body에 드래그 드랍했을 때
+body.addEventListener('drop', function (e) {
+    e.preventDefault();
+    console.log('drop');
+    //버튼 글자 'Upload File'에서 'Submit'으로 바꾸기
+});
 
 
-// //modal에 클릭 이벤트가 발생했을 때
-// modal.addEventListener('click', (event) => {
-//     if (event.target === modal) {//클릭이 모달창 위면
-//         modal.classList.toggle('show');//모달창을 띄우고
+//modal에 클릭 이벤트가 발생했을 때
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {//클릭이 모달창 위면
+        modal.classList.toggle('show');//모달창을 띄우고
 
-//         if (!modal.classList.contains('show')) {
-//             body.style.overflow = 'auto';
-//         }
-//     }
-// });
+        if (!modal.classList.contains('show')) {
+            body.style.overflow = 'auto';
+        }
+    }
+});
