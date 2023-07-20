ffb2088b7cccb7a2c19748430764d354bbfbb3b4
SeolJiyun <85937340+SeolJiyun@users.noreply.github.com>
2023-07-20 21:35:49 +0900

    err
    css, js파일 연결안됨

--- djangoproject/static/js/home.js
+++ djangoproject/static/js/home.js
@@ -6,12 +6,23 @@ const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 let file = document.getElementById("input-file")
 
-fileInput.addEventListener('input', (event) =>{
+// fileInput.addEventListener('input', (event) =>{
+//     const files = event.target.files;
+//     fileOutput.textContent = Array.from(files).map(file=> file.name).join("\n")
+//     fileBtn.setAttribute("hidden", "hidden");
+//     submitBtn.setAttribute("type", "submit");
+// })
+// 파일 입력 요소와 드래그 앤 드랍 이벤트를 처리하는 함수
+function handleFileSelection(event) {
     const files = event.target.files;
-    fileOutput.textContent = Array.from(files).map(file=> file.name).join("\n")
+    fileOutput.textContent = Array.from(files).map(file => file.name).join("\n");
     fileBtn.setAttribute("hidden", "hidden");
     submitBtn.setAttribute("type", "submit");
-})
+    body.backgroundColor = "#183e84";
+}
+
+// 파일 입력 요소 이벤트 리스너 등록
+fileInput.addEventListener('input', handleFileSelection);
 
 function handleBtnEnter(){
     fileBtn.style.backgroundColor = "#183e84";
@@ -35,28 +46,37 @@ fileBtn.addEventListener("mouseleave", handleBtnLeave);
 submitBtn.addEventListener("mouseenter", handleBtnEnter);
 submitBtn.addEventListener("mouseleave", handleBtnLeave);
 
-//파일 넣는 부분에 이벤트가 발생했을 때 
-file.onchange = function (e) {
-    modal.classList.toggle('show');//모달창을 띄운다.
-}
+// //파일 넣는 부분에 이벤트가 발생했을 때 
+// file.onchange = function (e) {
+//     modal.classList.toggle('show');//모달창을 띄운다.
+// }
 
 //body에 드래그하고 있을 때
 body.addEventListener('dragover', function (e) {
     e.preventDefault();
-    console.log('dragover');
+    console.log('dragoverrrr');
     modal.classList.add('show');//모달창을 띄운다. add가 아닌 toggle로 하면 모달창이 껏다 켜졌다 반복함.
 });
 
 //body에 드래그 드랍했을 때
 body.addEventListener('drop', function (e) {
     e.preventDefault();
-    console.log('drop');
+    console.log('bodydrop');
     //버튼 글자 'Upload File'에서 'Submit'으로 바꾸기
+    handleFileSelection(e);
+});
+
+//modal에 드래그 드랍했을 때
+modal.addEventListener('drop', function (e) {
+    console.log('modaldrop');
+    //버튼 글자 'Upload File'에서 'Submit'으로 바꾸기
+    handleFileSelection(e);
 });
 
 
 //modal에 클릭 이벤트가 발생했을 때
 modal.addEventListener('click', (event) => {
+    
     if (event.target === modal) {//클릭이 모달창 위면
         modal.classList.toggle('show');//모달창을 띄우고
 
