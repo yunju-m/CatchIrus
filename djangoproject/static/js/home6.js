549932c2b3e432e46873c8c9a1dee84bf3fd4188
SeolJiyun <85937340+SeolJiyun@users.noreply.github.com>
2023-08-07 22:42:44 +0900

    파일 전송 고민중

--- djangoproject/static/js/home6.js
+++ djangoproject/static/js/home6.js
@@ -5,23 +5,7 @@ const submitBtn = document.getElementById("submitBtn");
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const modal_post = document.getElementById("modal_post");
-let file; // 추가: 선택된 파일을 저장할 변수
-
-// 파일 선택 요소와 드래그 앤 드롭 이벤트를 처리하는 함수
-function handleFileSelection(event) {
-    const files = event.target.files || event.dataTransfer.files;
-
-    // 이미 선택된 파일이 있다면 기존 파일을 덮어쓰기
-    if (files.length > 0) {
-        file = files[0]; // 첫 번째 파일만 사용
-        fileOutput.textContent = file.name;
-        submitBtn.setAttribute("type", "submit");
-        fileBtn.setAttribute("hidden", "hidden");
-    }
-}
-
-// 파일 입력 요소 이벤트 리스너 등록
-fileInput.addEventListener('change', handleFileSelection);
+let file;
 
 // 드래그 앤 드롭 이벤트 처리
 function handleDropEvent(e) {
@@ -38,28 +22,30 @@ function handleDropEvent(e) {
     }
 
     modal.classList.remove('show'); // 드롭 후 모달창 닫기
-    // 이후의 파일 업로드 과정은 아래의 코드와 동일
+    //이후의 파일 업로드 과정은 아래의 코드와 동일
     
-    // // 서버로 파일 전송
-    // const formData = new FormData();
-    // formData.append("input-file", file);
+    //////////// 여기부터 파이썬에서 하는 일 같다고 추측 중
+    // 서버로 파일 전송
+    const formData = new FormData();
+    formData.append("input-file", file);
 
-    // fetch('/home/', {
-    //     method: 'POST',
-    //     body: formData,
-    //     headers: {
-    //         'X-CSRFToken': getCookie('csrftoken'), // Django CSRF 토큰을 헤더에 포함
-    //     },
-    // })
-    // .then(response => response.text())
-    // .then(data => {
-    //     console.log("xx");
-    //     // 파일 업로드가 성공적으로 완료되면 여기에 추가적인 동작을 수행합니다.
-    //     // 예: 서버로부터 받은 응답 데이터 처리 등
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    fetch('/home/', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-CSRFToken': getCookie('csrftoken'), // Django CSRF 토큰을 헤더에 포함
+        },
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log("xx");
+        // 파일 업로드가 성공적으로 완료되면 여기에 추가적인 동작을 수행합니다.
+        // 예: 서버로부터 받은 응답 데이터 처리 등
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    //////////////////////////////여기까지
 }
 
 // body에 드래그 앤 드롭 이벤트 리스너 등록
@@ -84,3 +70,7 @@ function handleBtnClick(event){
 }
 
 fileBtn.addEventListener("click", handleBtnClick);
+
+function rrr(){
+
+}
