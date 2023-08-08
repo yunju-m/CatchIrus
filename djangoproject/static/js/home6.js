dab5c5f13e72fc4393274dbcd1d23c8c5eba40de
SeolJiyun <85937340+SeolJiyun@users.noreply.github.com>
2023-08-08 23:19:44 +0900

    이어서..

--- djangoproject/static/js/home6.js
+++ djangoproject/static/js/home6.js
@@ -5,7 +5,23 @@ const submitBtn = document.getElementById("submitBtn");
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const modal_post = document.getElementById("modal_post");
-let file;
+let file; // 추가: 선택된 파일을 저장할 변수
+
+// 파일 선택 요소와 드래그 앤 드롭 이벤트를 처리하는 함수
+function handleFileSelection(event) {
+    const files = event.target.files || event.dataTransfer.files;
+
+    // 이미 선택된 파일이 있다면 기존 파일을 덮어쓰기
+    if (files.length > 0) {
+        file = files[0]; // 첫 번째 파일만 사용
+        fileOutput.textContent = file.name;
+        submitBtn.setAttribute("type", "submit");
+        fileBtn.setAttribute("hidden", "hidden");
+    }
+}
+
+// 파일 입력 요소 이벤트 리스너 등록
+fileInput.addEventListener('change', handleFileSelection);
 
 // 드래그 앤 드롭 이벤트 처리
 function handleDropEvent(e) {
@@ -23,7 +39,7 @@ function handleDropEvent(e) {
     }
     //modal_post.setAttribute("method","POST");
     modal.classList.remove('show'); // 드롭 후 모달창 닫기
-    //이후의 파일 업로드 과정은 아래의 코드와 동일
+    // 이후의 파일 업로드 과정은 아래의 코드와 동일
     
     //////////// 여기부터 파이썬에서 하는 일 같다고 추측 중
     // // 서버로 파일 전송
@@ -32,6 +48,7 @@ function handleDropEvent(e) {
     // console.log("ddd");
 
     // fetch('/modalfile', {
+
     //     method: 'POST',
     //     body: formData,
     //     headers: {
@@ -40,13 +57,10 @@ function handleDropEvent(e) {
     // })
     // .then(response => response.text())
     // .then(data => {
-        
     // })
     // .catch(error => {
     //     console.error('Error:', error);
     // });
-    // //////////////////////////////여기까지
-    
 }
 
 // body에 드래그 앤 드롭 이벤트 리스너 등록
@@ -71,3 +85,4 @@ function handleBtnClick(event){
 }
 
 fileBtn.addEventListener("click", handleBtnClick);
+
